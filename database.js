4cc3a4fc736942e6df1c575c1c6f5dfba3bf099c
Rob <snowjunkierob@gmail.com>
2021-09-02 13:13:10 -0600

    route remains complete, k6 test suite needs to be built

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
-const { dbPass } = require('./config');
-
 const initOptions = {
   schema: 'overview_schema',
 };
 const pgp = require('pg-promise')(initOptions);
+const { dbPass } = require('./config');
+
 const cn = `postgres://rob:${dbPass}@localhost:5432/SDC_overview_service`;
 const db = pgp(cn);
 
