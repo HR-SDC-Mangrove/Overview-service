8754431382b237b72bfc3e636ab028183d8e9de1
Rob <snowjunkierob@gmail.com>
2021-09-23 14:16:09 -0600

    created reader scripts that allow for transformation of data from CSV to JSON objects, that can be uploaded to mongo (for single schema implementation). added helper.js - includes two empty functions.  added tests/helper.js - includes 60 tests to ensure the correct data is sent to client.

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,19 +3,19 @@ const helper = require('./helper');
 
 module.exports = {
   products: (req, res) => {
-    const inputId = req.url.split('=')[1];
-    // console.log(req)
+    // const inputId = req.url.split('=')[1];
+    // // console.log(req)
 
-    db.models.Product.find({ id: inputId })
-      .then((product) => {
-        // console.log(product)
-        res.send(product[0])
-        // console.log(result[0].styles)
-        // res.send(result[0]);
-      })
-      .catch((err) => {
-        console.log(err);
-        res.send(400);
-      });
+    // db.models.Product.find({ id: inputId })
+    //   .then((product) => {
+    //     // console.log(product)
+    //     res.send(product[0])
+    //     // console.log(result[0].styles)
+    //     // res.send(result[0]);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     res.send(400);
+    //   });
   },
 };
